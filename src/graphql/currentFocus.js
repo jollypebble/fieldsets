c659bf3c102717533b8c133389cfd33d7f7dcbd9
William Gladstone <will@jollypebble.com>
2019-01-04 16:57:08 -0500

    successfully write to apollo cache

--- src/graphql/currentFocus.js
+++ src/graphql/currentFocus.js
@@ -1,1 +1,16 @@
 import gql from 'graphql-tag';
+
+export const getCurrentFocus = gql`
+  query GetCurrentFocus {
+    currentFocus @client {
+      currentX
+      currentY
+    }
+  }
+`;
+
+export const getCurrentFocus = ({
+  props: ({ data: { currentFocus } }) => ({
+    currentFocus
+  })
+});
