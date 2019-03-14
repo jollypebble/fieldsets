129ce44242c71b4db65a046a499dddd71051f2f9
admin <mobile.com21@yahoo.com>
2019-03-14 21:01:48 +0100

    added Net Worth node

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { withApollo } from 'react-apollo';
+import { withApollo } from "react-apollo";
+import ReactGoogleSheets from 'react-google-sheets';
+
+import { Auth, Config } from './config';
 
 // Custom Components
 import { Dashboard } from './containers/UI'
@@ -26,7 +29,6 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log('App did mount')
     Definder.init()
   }
 
@@ -40,13 +42,19 @@ class App extends Component {
 
     return (
       <React.Fragment>
+        <ReactGoogleSheets
+          clientId={Auth.google.web.client_id}
+          apiKey={Config.apiKey}
+          spreadsheetId={Config.sheetID}
+          afterLoading={() => this.setState({sheetLoaded: true})}
+        />
         <div id="econcircleapp">
           <CircleDiagram
             width={diagramWidth}
             height={1080}
             zoom={diagramZoom}
             startX={30.8 + (diagramWidth - screenWidth) / diagramZoom * 0.5} // here we calculate the point which means the center of the screen (the first "magic number" is unknown shift which puts "0" of diagram to "0" of the screen)
-            startY={46}
+            startY={50}
           />
         </div>
         <Dashboard />
--- src/containers/Diagrams/CircleDiagram/CircleDiagram.jsx
+++ src/containers/Diagrams/CircleDiagram/CircleDiagram.jsx
@@ -4,7 +4,7 @@ import { DiagramData, FieldData } from '../../../config';
 import { ReactSVGPanZoom } from 'react-svg-pan-zoom';
 
 //import { Diagram, DiagramCache } from 'containers/Diagrams/Diagram';
-import { RadialNode, RadialDialog } from '../../../components/Diagrams';
+import { NetWorthNode, RadialNode, RadialDialog } from '../../../components/Diagrams';
 import { getFields, getCurrentFocus, getNodes, defaults } from '../../../graphql';
 
 /**
@@ -276,7 +276,7 @@ class CircleDiagram extends Component {
     } = this.state;
 
     // Scale our SVG based on our desired width height based on a 100 x 75 canvas.
-    const baseradius = 2;
+    const baseradius = 2.3;
     const radius = (baseradius*75)/100;
 
     return (
@@ -302,15 +302,16 @@ class CircleDiagram extends Component {
                 id="circlediagram" width={width} height={height}
               >
                 <g id="diagramGroup">
-                  { DiagramData.map(diagram => (
-                    <RadialNode
+                  { DiagramData.map(diagram => {
+                    let NodeClass = RadialNode
+                    if (diagram.id === 'net_worth') NodeClass = NetWorthNode
+                    return <NodeClass
                       key={ diagram.id }
                       nodeData={ typeof(diagram.children) === undefined ? [] : diagram.children }
                       nodeID={ diagram.id }
                       centerX={ diagram.centerX }
                       centerY={ diagram.centerY }
                       radius={ radius }
-                      visible={ typeof(diagram.visible) === 'undefined' ? true : diagram.visible }
                       name={ diagram.name }
                       parent={ diagram.parent }
                       fields={ diagram.fields }
@@ -322,7 +323,7 @@ class CircleDiagram extends Component {
                       nodes={this.state.nodes}
                       isShown={true /* The prop means whether the node is being rendered right now by its parent */}
                     />
-                  )) }
+                  }) }
                 </g>
               </svg>
             </ReactSVGPanZoom>
