644bbcc2f5757389210483612f020b9f8c203c1c
William Gladstone <will@jollypebble.com>
2019-04-02 11:24:21 -0400

    placeholder

--- src/containers/Diagrams/CircleDiagram/CircleDiagram.jsx
+++ src/containers/Diagrams/CircleDiagram/CircleDiagram.jsx
@@ -190,7 +190,9 @@ class CircleDiagram extends Component {
   }
 
   getFieldData = (variables) => {
-    return this.props.client.readQuery({ query: getParentFields, variables: variables });
+    console.log(variables);
+    const id = `Field:${variables.id}`;
+    return this.props.client.readFragment({ id: id, query: getParentFields, variables: variables });
   }
 
   getNodesData = (variables) => {
@@ -253,7 +255,8 @@ class CircleDiagram extends Component {
 
   setFieldCache = (data=[]) => {
     // console.log('Updating Field cache');
-    let previous = this.getAllFields({});
+    let previous = this.getAllFields();
+    console.log(previous.fields);
     if (!data.length) return false;
 
     // Get your fields here. This is defined as a static json, but could be modified here to get remote field type definitions.
@@ -292,7 +295,7 @@ class CircleDiagram extends Component {
         this.setDataCache(children);
       }
 
-      const previous = this.getFieldData({parent: node.id});
+      const previous = this.getFieldData({id: node.id});
       console.log('Previous fields:', previous.fields);
       const fields = previous.fields.map(field => {
         field.__typename = 'Field';
