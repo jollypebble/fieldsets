6a801a2df1de2752bb842670b69b5cbff18cd2d8
superWsac <sacred3091122@gmail.com>
2019-05-10 19:10:31 +0300

    update overall svg ui with a new mockup

--- src/components/Diagrams/Node.jsx
+++ src/components/Diagrams/Node.jsx
@@ -80,7 +80,6 @@ class Node extends React.Component {
 
   /** Is called when the cursor acrosses borders of the radial node getting inside of it */
   handleMouseEnter() {
-    if (this.props.id === 'mid_term_money') return;
     this.setState({ isMouseInside: true });
   }
 
