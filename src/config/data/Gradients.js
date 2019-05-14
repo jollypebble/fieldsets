15876659d3e5fee19371e3e4462eb379a5fb0dee
superWsac <sacred3091122@gmail.com>
2019-05-14 10:20:24 +0300

    Add round svg for parent model

--- src/config/data/Gradients.js
+++ src/config/data/Gradients.js
@@ -17,61 +17,77 @@ export const netWorth = () => (
 );
 
 export const defenceAllocation = () => (
-  <radialGradient
-    id="defenceAllocation"
-    gradientUnits="userSpaceOnUse"
-    gradientTransform="matrix(1.0527117,0.00377601,-0.00186585,0.52017531,-4.3193509,34.654517)"
-    r="25.435919"
-    fy="6.1103106"
-    fx="52.520226"
-    cx="52.520226"
-    cy="6.1103106"
-  >
-    <stop offset="0%" stopColor="#f17372" />
-    <stop offset="100%" stopColor="#fdeaeb" />
-  </radialGradient>
+  // <radialGradient
+  //   id="defenceAllocation"
+  //   gradientUnits="userSpaceOnUse"
+  //   gradientTransform="matrix(1.0527117,0.00377601,-0.00186585,0.52017531,-4.3193509,34.654517)"
+  //   r="25.435919"
+  //   fy="6.1103106"
+  //   fx="52.520226"
+  //   cx="52.520226"
+  //   cy="6.1103106"
+  // >
+  //   <stop offset="0%" stopColor="#f17372" />
+  //   <stop offset="100%" stopColor="#fdeaeb" />
+  // </radialGradient>
+  <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="defenceAllocation">
+    <stop stop-color="#fdeaeb" stop-opacity="0.5" offset="0%"></stop>
+    <stop stop-color="#f17372" stop-opacity="0.5" offset="100%"></stop>
+  </linearGradient>
 );
 
 export const longTermMoney = () => (
-  <radialGradient
-    id="longTermMoney"
-    cx="48.952183"
-    cy="46.994781"
-    fx="48.952183"
-    fy="46.994781"
-    r="17.334031"
-    gradientTransform="matrix(0.2237759,1.685363,-1.9036629,0.25276092,129.4658,-49.188762)"
-    gradientUnits="userSpaceOnUse"
-  >
-    <stop offset="0%" stopColor="#4fc6f5" />
-    <stop offset="100%" stopColor="#ddf5ff" />
-  </radialGradient>
+  // <radialGradient
+  //   id="longTermMoney"
+  //   cx="48.952183"
+  //   cy="46.994781"
+  //   fx="48.952183"
+  //   fy="46.994781"
+  //   r="17.334031"
+  //   gradientTransform="matrix(0.2237759,1.685363,-1.9036629,0.25276092,129.4658,-49.188762)"
+  //   gradientUnits="userSpaceOnUse"
+  // >
+  //   <stop offset="0%" stopColor="#4fc6f5" />
+  //   <stop offset="100%" stopColor="#ddf5ff" />
+  // </radialGradient>
+  <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="longTermMoney">
+    <stop stop-color="#4fc6f5" stop-opacity="0.5" offset="0%"></stop>
+    <stop stop-color="#ddf5ff" stop-opacity="0.5" offset="100%"></stop>
+  </linearGradient>
 );
 
 export const midTermMoney = () => (
-  <linearGradient
-    id="midTermMoney"
-    x1="50.655983"
-    y1="49.904922"
-    x2="47.819263"
-    y2="75.616295"
-    gradientUnits="userSpaceOnUse"
-  >
-    <stop offset="0%" stopColor="#89c8e5" />
-    <stop offset="100%" stopColor="#eff8fb" />
+  // <linearGradient
+  //   id="midTermMoney"
+  //   x1="50.655983"
+  //   y1="49.904922"
+  //   x2="47.819263"
+  //   y2="75.616295"
+  //   gradientUnits="userSpaceOnUse"
+  // >
+  //   <stop offset="0%" stopColor="#89c8e5" />
+  //   <stop offset="100%" stopColor="#eff8fb" />
+  // </linearGradient>
+  <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="midTermMoney">
+    <stop stop-color="#eff8fb" stop-opacity="0.5" offset="0%"></stop>
+    <stop stop-color="#89c8e5" stop-opacity="0.5" offset="100%"></stop>
   </linearGradient>
 );
 
 export const shortTermMoney = () => (
-  <linearGradient
-    id="shortTermMoney"
-    x1="48.160332"
-    y1="45.586319"
-    x2="24.940178"
-    y2="56.928658"
-    gradientUnits="userSpaceOnUse"
-  >
-    <stop offset="0%" stopColor="#b8e7f5" />
-    <stop offset="100%" stopColor="#f7fcff" />
+  // <linearGradient
+  //   id="shortTermMoney"
+  //   x1="48.160332"
+  //   y1="45.586319"
+  //   x2="24.940178"
+  //   y2="56.928658"
+  //   gradientUnits="userSpaceOnUse"
+  // >
+  //   <stop offset="0%" stopColor="#b8e7f5" />
+  //   <stop offset="100%" stopColor="#f7fcff" />
+  // </linearGradient>
+  <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="shortTermMoney">
+    <stop stop-color="#b8e7f5" stop-opacity="1" offset="0%"></stop>
+    <stop stop-color="#f7fcff" stop-opacity="1" offset="100%"></stop>
   </linearGradient>
 );
